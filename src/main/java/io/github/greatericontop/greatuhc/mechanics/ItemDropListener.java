fce365dcb370bc9301aa39a58b4b6e6c797cf7e1
greatericontop <greateric@protonmail.com>
2023-01-13 18:29:57 -0500

    mining ores changes

--- src/main/java/io/github/greatericontop/greatuhc/mechanics/ItemDropListener.java
+++ src/main/java/io/github/greatericontop/greatuhc/mechanics/ItemDropListener.java
@@ -39,20 +39,27 @@ public class ItemDropListener implements Listener {
                 event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.FLINT, 1));
             }
         }
+
         if (typeBroken == Material.IRON_ORE || typeBroken == Material.DEEPSLATE_IRON_ORE) {
             event.setDropItems(false);
-            int amount = Math.random() < 0.2 ? 2 : 1;
-            amount += (plugin.uhcMiningModifier && Math.random() < 0.5) ? 1 : 0;
+            int amount = Math.random() < 0.15 ? 2 : 1;
+            amount += (plugin.uhcMiningModifier && Math.random() < 0.4) ? 1 : 0;
             event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, amount));
             event.setExpToDrop(randint(1, 2));
         }
         if (typeBroken == Material.GOLD_ORE || typeBroken == Material.DEEPSLATE_GOLD_ORE) {
             event.setDropItems(false);
-            int amount = Math.random() < 0.2 ? 2 : 1;
-            amount += (plugin.uhcMiningModifier && Math.random() < 0.5) ? 1 : 0;
+            int amount = Math.random() < 0.15 ? 2 : 1;
+            amount += (plugin.uhcMiningModifier && Math.random() < 0.4) ? 1 : 0;
             event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.GOLD_INGOT, amount));
             event.setExpToDrop(randint(1, 3));
         }
+        if (typeBroken == Material.DIAMOND_ORE || typeBroken == Material.DEEPSLATE_DIAMOND_ORE) {
+            if (plugin.uhcMiningModifier && Math.random() < 0.4) {
+                event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.DIAMOND, 1));
+            }
+        }
+
         if (typeBroken == Material.OAK_LEAVES || typeBroken == Material.DARK_OAK_LEAVES) {
             if (Math.random() < 0.035) {
                 event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.APPLE, 1));
