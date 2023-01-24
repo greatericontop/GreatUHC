cf3219056a7b927a115ce315d02a9b51ef21aa94
greatericontop <greateric@protonmail.com>
2023-01-24 18:41:03 -0500

    axe nerf now applies to all axes instead of only crafted ones

--- src/main/java/io/github/greatericontop/greatuhc/mechanics/OldPVP.java
+++ src/main/java/io/github/greatericontop/greatuhc/mechanics/OldPVP.java
@@ -1,34 +1,34 @@
 package io.github.greatericontop.greatuhc.mechanics;
 
 import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.UUID;
 
 public class OldPVP implements Listener {
 
     @EventHandler()
     public void onCraft(CraftItemEvent event) {
         Material resultType = event.getRecipe().getResult().getType();
-        if (resultType == Material.WOODEN_AXE || resultType == Material.STONE_AXE || resultType == Material.IRON_AXE || resultType == Material.GOLDEN_AXE || resultType == Material.DIAMOND_AXE) {
-            ItemStack newResult = event.getInventory().getResult();
-            ItemMeta im = newResult.getItemMeta();
-            im.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "OldPVP", 0.01, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
-            newResult.setItemMeta(im);
-            event.getInventory().setResult(newResult);
-        }
         if (resultType == Material.SHIELD) {
             event.getWhoClicked().sendMessage("§cShields are not available in UHC.");
             event.setCancelled(true);
         }
 
+    }
+
+    @EventHandler()
+    public void onPlayerAttack(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player player))  return;
+        ItemStack meleeItem = player.getInventory().getItemInMainHand();
+        Material mat = meleeItem.getType();
+        if (mat == Material.WOODEN_AXE || mat == Material.STONE_AXE || mat == Material.IRON_AXE || mat == Material.GOLDEN_AXE || mat == Material.DIAMOND_AXE || mat == Material.NETHERITE_AXE) {
+            event.setDamage(event.getDamage() * 0.5);
+            player.sendActionBar("§cYou shouldn't use axes to attack in UHC.");
+        }
     }
 
 }
