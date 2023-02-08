4907688a7804cacee74a75f3c122d82581e83016
greatericontop <greateric@protonmail.com>
2023-02-08 16:51:04 -0500

    disallowing anvil also disallows it in the sacrifice slot

--- src/main/java/io/github/greatericontop/greatuhc/mechanics/AntiAnvil.java
+++ src/main/java/io/github/greatericontop/greatuhc/mechanics/AntiAnvil.java
@@ -13,21 +13,25 @@ public class AntiAnvil implements Listener {
     private static final NamespacedKey NO_ANVIL_KEY = new NamespacedKey("uhc", "noanvil");
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void onAnvil(PrepareAnvilEvent event) {
+    public void onAnvilSource(PrepareAnvilEvent event) {
         ItemStack source = event.getInventory().getItem(0);
         ItemStack sacrifice = event.getInventory().getItem(1);
         if (source == null)  return;
         // only disable anvil when there is also no sacrifice item
         // if there's no sacrifice item you can only rename, which is ok
         if (sacrifice == null)  return;
-        ItemMeta im = source.getItemMeta();
-        if (im == null)  return;
-        if (im.getPersistentDataContainer().has(NO_ANVIL_KEY, PersistentDataType.INTEGER)) {
+        ItemMeta imSource = source.getItemMeta();
+        ItemMeta imSacrifice = sacrifice.getItemMeta();
+        if (isAnvilDisabled(imSource) || isAnvilDisabled(imSacrifice)) {
             // I COULD try setting this to an error item, but that requires checking InventoryClickEvent
             // to delete it if someone actually takes it, and I'm too lazy for that.
             event.setResult(null);
             event.getViewers().get(0).sendMessage("§cYou can't use this in an anvil!");
         }
+    }
+
+    private boolean isAnvilDisabled(ItemMeta im) {
+        return im != null && im.getPersistentDataContainer().has(NO_ANVIL_KEY, PersistentDataType.INTEGER);
     }
 
     public static void disallowAnvil(ItemMeta im) {
