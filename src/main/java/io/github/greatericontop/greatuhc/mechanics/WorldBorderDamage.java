6843b50d61a1cf750c28ffd9741f12718352b72b
greatericontop <greateric@protonmail.com>
2023-02-19 09:27:45 -0500

    don't damage creative and spectator players

--- src/main/java/io/github/greatericontop/greatuhc/mechanics/WorldBorderDamage.java
+++ src/main/java/io/github/greatericontop/greatuhc/mechanics/WorldBorderDamage.java
@@ -3,6 +3,7 @@ package io.github.greatericontop.greatuhc.mechanics;
 import io.github.greatericontop.greatuhc.GreatUHCMain;
 import io.github.greatericontop.greatuhc.util.TrueDamageHelper;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -12,6 +13,7 @@ public class WorldBorderDamage {
         new BukkitRunnable() {
             public void run() {
                 for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR)  continue;
                     if (!player.getWorld().getWorldBorder().isInside(player.getLocation())) {
                         player.sendActionBar("§cYou are outside the world; turn back!");
                         TrueDamageHelper.dealTrueDamage(player, 1.0);
