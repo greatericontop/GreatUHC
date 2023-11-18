ffb59a9b7d818dfb96c7c301c83bb4f9934ecae6
greatericontop <greateric@protonmail.com>
2023-11-18 16:32:35 -0500

    i forgot i used it here

--- src/main/java/io/github/greatericontop/greatuhc/customitems/PeriodicItemUpgradeListener.java
+++ src/main/java/io/github/greatericontop/greatuhc/customitems/PeriodicItemUpgradeListener.java
@@ -41,6 +41,8 @@ public class PeriodicItemUpgradeListener implements Listener {
         ItemMeta im = mainHandItem.getItemMeta();
         if (im == null)  return;
 
+        int pvpTime = gameManager.getPlugin().getConfig().getInt("pvp_time");
+
         if (im.getPersistentDataContainer().has(new NamespacedKey("uhc", "apprentice_sword"), PersistentDataType.INTEGER)) {
             int previousLevel = im.getEnchantLevel(Enchantment.DAMAGE_ALL);
             im.removeEnchant(Enchantment.DAMAGE_ALL);
@@ -49,7 +51,7 @@ public class PeriodicItemUpgradeListener implements Listener {
                 if (previousLevel != 3) {
                     event.getPlayer().sendMessage("§aYour §cApprentice Sword §areceived Sharpness III");
                 }
-            } else if (gameManager.getCurrentPhase() == GameManager.GamePhase.PVP && gameManager.getTicksLeft() < GameManager.PVP_TIME-12000) {
+            } else if (gameManager.getCurrentPhase() == GameManager.GamePhase.PVP && gameManager.getTicksLeft() < pvpTime-12000) {
                 im.addEnchant(Enchantment.DAMAGE_ALL, 2, true);
                 if (previousLevel != 2) {
                     event.getPlayer().sendMessage("§aYour §cApprentice Sword §areceived Sharpness II");
@@ -71,7 +73,7 @@ public class PeriodicItemUpgradeListener implements Listener {
                 if (previousLevel != 2) {
                     event.getPlayer().sendMessage("§aYour §cApprentice Bow §areceived Power II");
                 }
-            } else if (gameManager.getCurrentPhase() == GameManager.GamePhase.PVP && gameManager.getTicksLeft() < GameManager.PVP_TIME-12000) {
+            } else if (gameManager.getCurrentPhase() == GameManager.GamePhase.PVP && gameManager.getTicksLeft() < pvpTime-12000) {
                 im.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
                 if (previousLevel != 1) {
                     event.getPlayer().sendMessage("§aYour §cApprentice Bow §areceived Power I");
